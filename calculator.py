75c31fdc7761b250c5da39a68a4640f66711558e
zfreitas <Zachariah.Freitas@viasat.com>
2022-10-28 12:07:18 -0700

    Added code

---
+++ calculator.py
@@ -0,0 +1,5 @@
+x = int(input("What's x? "))
+# var = input("Enter One \"+, -, / ,* , %\"? ")
+y = int(input("What's y? "))
+
+print((x + y))
--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
 # Ask the user for their name.
-name = input("What's your name? ")
+# Remove Whitespace from string and Use title case the string.
+name = (
+    input("What's your name? ")
+    .strip() # Strip extra spaces
+    .title() # Use Propercase
+    )
+
+# Split users name into first and last name
+first, last = name.split(" ")
 
 # Say hello to user.
-print(f"\nHi {name}!")
-print("\nHi " + name + '!')
-print("\nHi", + name + '!')
+print(f"\nHi {first}!")
+print("\nHi " + name + "!")
+print("\nHi", name, "!")
